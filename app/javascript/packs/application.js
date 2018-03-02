92a95a73e77cf604654210416dea5ab1951d6064
pixelgoo <pixelgoo@gmail.com>
2018-03-02 20:43:04 +0200

    Initial project: Rails 5.1, Webpacker (no assets pipeline), jQuery, Postgresql

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,17 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+require.context('../stylesheets/', true, /^\.\/[^_].*\.(css|scss|sass)$/i)
+require.context('../images/', true, /\.(gif|jpg|jpeg|png|svg)$/i)
+
+import $ from 'jquery'
+global.$ = $
+global.jQuery = $
+
+console.log('Webpacker succesfully compiled assets!')
