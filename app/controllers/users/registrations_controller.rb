f7e5f4ca6c7b96e674187f813e43a3b9c49e280f
vagrant <vagrant@vagrant.vm>
2018-03-13 23:18:37 +0000

    Add navigation login and log out

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  
   def after_sign_up_path_for(resource)
-    root_path(resource)
+    root_path
   end
 
 end
