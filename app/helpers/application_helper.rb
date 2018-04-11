43a8d72a461360869eaa5cc7f2d89e584a5b96ca
pixelgoo <pixelgoo@gmail.com>
2018-04-11 15:17:07 +0200

    :bug: Fix product image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,8 +11,8 @@ module ApplicationHelper
 
     def product_image(product)
         %w(jpg png webp).each do |extension|
-            image_path = Pathname.new "#{Rails.root}/app/webpack/images/products/#{product.category_id}/#{product.product_id}/#{product.product_id}_0.#{extension}"
-            return image_tag(asset_pack_path("images/products/#{product.category_id}/#{product.product_id}/#{product.product_id}_0.#{extension}")) if image_path.exist?
+            image_path = Pathname.new "#{Rails.root}/app/webpack/images/products/#{product.category_id}/#{product.product_id}_0.#{extension}"
+            return image_tag(asset_pack_path("images/products/#{product.category_id}/#{product.product_id}_0.#{extension}")) if image_path.exist?
         end
     end
 
