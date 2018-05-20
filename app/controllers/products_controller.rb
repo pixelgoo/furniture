835a537f9648a50de62993723a177ddcb4f3fe48
pixelgoo <pixelgoo@gmail.com>
2018-05-20 20:14:23 +0200

    Filters styling + add scopes for Product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,16 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
+  
+  has_scope :furniture
+  has_scope :category
+  has_scope :factory
+  has_scope :style
+  has_scope :facade
+  has_scope :structure
+  has_scope :product_type
 
   def index
-    @products = Product.page(params[:page]).per(10)
+    @products = apply_scopes(Product).all.page(params[:page]).per(10)
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ApplicationRecord
     belongs_to :category
     has_many :textiles
     has_many :requests
+    
+    scope :furniture, -> furniture { Furniture.where(id: furniture).first.products }
+    scope :category, -> category { where category_id: category }
+    scope :factory, -> factory { where factory: factory }
+    scope :style, -> style { where style: style }
+    scope :facade, -> { where facade: facade }
+    scope :structure, -> structure { where structure: structure }
+    scope :product_type, -> product_type { where product_type: product_type}
 end
